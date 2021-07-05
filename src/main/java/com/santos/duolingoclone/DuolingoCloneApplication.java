a7f96785a861f073fcd0bcd3dd5593e8e65a8942
isaiassantossilva <isaias661248@gmail.com>
2021-07-05 10:59:16 -0300

    first commit

---
+++ src/main/java/com/santos/duolingoclone/DuolingoCloneApplication.java
@@ -0,0 +1,13 @@
+package com.santos.duolingoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuolingoCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuolingoCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santos/duolingoclone/DuolingoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.duolingoclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuolingoCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
